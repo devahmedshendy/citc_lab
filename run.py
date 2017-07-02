a7e2b3946bbdea691628bb0082815146850151ec
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2017-07-02 09:37:13 +0200

    Change run.py to accept port from environment variable

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from app import app
 
+import os
+
+if os.environ["PORT"]:
+  port = int(os.environ["PORT"])
+
+else:
+  port = 5000
+
 app.debug = True
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=port)
