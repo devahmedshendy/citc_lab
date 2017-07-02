c86a21fbff0f8f5e1a7feada5e22e308db63d6ce
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2017-07-02 10:37:54 +0200

    Fix typo

--- config/db_config_template.py
+++ config/db_config_template.py
@@ -22,7 +22,8 @@ CLEARDB     = {  "DRIVER"  : 'mysql+mysqldb',
                  "NAME"    : 'heroku_9c5fb999c980059',
                  "HOST"    : 'us-cdbr-iron-east-03.cleardb.net',
                  "USER"    : 'bdbdf83b56d5d1',
-                 "PASSWD"  : 'ae7444b9',
+                 "PASSWD"  : 'ae7444b9' }
+
 CLEARDB_URI = '{}://{}:{}@{}/{}'.format(CLEARDB["DRIVER"],
                                 CLEARDB["USER"], CLEARDB["PASSWD"],
                                 CLEARDB["HOST"], CLEARDB["NAME"])
